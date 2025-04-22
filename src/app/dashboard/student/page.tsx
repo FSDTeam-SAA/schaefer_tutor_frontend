a76faa1d6a1da542d4ed8d255b63f092dad7c849
Monir Hossain <monirhrabby.personal@gmail.com>
2025-04-22 17:48:46 +0600

    developed: student dashboard

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -1,5 +1,63 @@
-const SchuelerDashboard = () => {
-  return <div>OverView is working...</div>;
+import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import StudentDashboardStats from "./_components/student-dashboard-stats";
+
+const SchuelerDashboard = async () => {
+  const cu = await auth();
+
+  if (!cu?.user.id) redirect("/login");
+
+  const now = new Date();
+
+  // Start of today (00:00:00)
+  const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+  // Start of tomorrow (00:00:00 of the next day)
+  const startOfTomorrow = new Date(startOfDay);
+  startOfTomorrow.setDate(startOfDay.getDate() + 1);
+
+  const todayLesson = await prisma.lesson.count({
+    where: {
+      studentId: cu.user.id,
+      date: {
+        gte: startOfDay,
+        lt: startOfTomorrow,
+      },
+    },
+  });
+  const totalCompletedLesson = await prisma.lesson.count({
+    where: {
+      studentId: cu.user.id,
+      status: "carried_out",
+    },
+  });
+  const totalPlannedLesson = await prisma.lesson.count({
+    where: {
+      studentId: cu.user.id,
+      status: "planned",
+    },
+  });
+
+  const pricing = await prisma.user.findFirst({
+    where: {
+      id: cu.user.id,
+    },
+    include: {
+      pricing: true,
+    },
+  });
+
+  return (
+    <div>
+      <StudentDashboardStats
+        todaysLessons={todayLesson}
+        completedLessons={totalCompletedLesson}
+        plannedSessions={totalPlannedLesson}
+        pricing={pricing?.pricing || undefined}
+      />
+    </div>
+  );
 };
 
 export default SchuelerDashboard;
