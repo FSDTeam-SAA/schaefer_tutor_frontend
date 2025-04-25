8e158230cb763d1b7c5486a4b5797e6992dc9bc5
Monir Hossain <monirhrabby.personal@gmail.com>
2025-04-25 07:01:08 +0600

    developed: sd

--- src/app/dashboard/student/payment/_components/cards/card-connet-remove-container.tsx
+++ src/app/dashboard/student/payment/_components/cards/card-connet-remove-container.tsx
@@ -1,47 +1,38 @@
 "use client";
-import { removePaymentMethod } from "@/action/payment-card";
 import { Button } from "@/components/ui/button";
-import { Loader2, Wallet } from "lucide-react";
+import { Wallet } from "lucide-react";
 import Link from "next/link";
-import { useTransition } from "react";
-import { toast } from "sonner";
 
 interface Props {
   pmid?: string | null;
 }
 
 const CardConnectRemoveContainer = ({ pmid }: Props) => {
-  const [isPending, startTransition] = useTransition();
+  // const onRemove = () => {
+  //   startTransition(() => {
+  //     removePaymentMethod().then((res) => {
+  //       if (!res.success) {
+  //         toast.error(res.message);
+  //         return;
+  //       }
 
-  const onRemove = () => {
-    startTransition(() => {
-      removePaymentMethod().then((res) => {
-        if (!res.success) {
-          toast.error(res.message);
-          return;
-        }
-
-        // handle success
-        toast.success(res.message);
-      });
-    });
-  };
+  //       // handle success
+  //       toast.success(res.message);
+  //     });
+  //   });
+  // };
   return (
     <div>
       {pmid ? (
-        <Button variant="destructive" onClick={onRemove} disabled={isPending}>
-          {isPending ? (
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <Wallet className="mr-2 h-4 w-4" />
-          )}{" "}
-          Remove Card
+        <Button variant="outline">
+          <Wallet className="mr-2 h-4 w-4" />
+          Wallet Connected
         </Button>
       ) : (
-        <Link href="/dashboard/student/payment/new">
+        <Link href="/dashboard/student">
           <Button>
             <Wallet className="mr-2 h-4 w-4" />
-            Connect Card
+            Buy Subscription
           </Button>
         </Link>
       )}
