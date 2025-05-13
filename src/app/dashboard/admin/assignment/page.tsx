30ce0313f68423c07d7899ffb3646b30d90c71a5
Monir Hossain <monirhrabby.personal@gmail.com>
2025-05-13 17:34:21 +0600

    a

--- src/app/dashboard/admin/assignment/page.tsx
+++ src/app/dashboard/admin/assignment/page.tsx
@@ -1,7 +1,30 @@
-const Page = () => {
+import { prisma } from "@/lib/prisma";
+import { AssignmentTableColumn } from "./_components/assignement-table-column";
+import AssignmentTableContainer from "./_components/assignment-table-container";
+
+const Page = async () => {
+  const allRequests = await prisma.connection.findMany({
+    where: {
+      status: {
+        in: ["pending", "rejected"],
+      },
+    },
+    include: {
+      teacher: true,
+      student: true,
+    },
+  });
+
   return (
     <div>
-      <h1 className="text-3xl font-bold">Pricing Management</h1>
+      <h1 className="text-3xl font-bold">Assignment</h1>
+
+      <div>
+        <AssignmentTableContainer
+          data={allRequests ?? []}
+          columns={AssignmentTableColumn}
+        />
+      </div>
     </div>
   );
 };
